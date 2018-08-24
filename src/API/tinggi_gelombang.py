d7ed3ec4832a3ea310a9d832140aadef91a60839
misdan <misdan@localhost.localdomain>
2018-08-24 14:38:05 +0700

    ditambah data tinggi gelombang

---
+++ src/API/tinggi_gelombang.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import sys, json
+import base64
+import datetime
+import time
+import os
+
+#buka file
+ds = xr.open_dataset('/home/misdan/Documents/Data/Tinggi Gelombang/1992/at_swh_sst_1992_01.nc')
+
+# Load JSON
+try:
+    data = json.loads(base64.b64decode(sys.argv[1]))
+except:
+    print "ERROR"
+    sys.exit(1)
+
+#Baca data
+a_temp = data[0]
+b_temp = data[1]
+c_temp = data[2]
+
+a = float(a_temp)
+b = float(b_temp)
+c = str(c_temp)
+s_datetime = datetime.datetime.strptime(c, '%Y%m%d')
+
+#Cari data
+data_nilai = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').data.tolist()
+data_tgl = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').time.data.astype(str).tolist()
+data_longitude = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').longitude.data.tolist()
+data_latitude = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').latitude.data.tolist()
+data_nama = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').long_name
+data_unit = ds['sst'].sel(time=s_datetime,latitude=a, longitude=b,method='nearest').units
+
+# Generate some data to send to PHP
+result = {'nilai': data_nilai,
+'tanggal': data_tgl,
+'longitude': data_longitude,
+'latitude': data_latitude,
+'detail_nama': data_nama,
+'detail_satuan': data_unit
+}
+
+# Send it to stdout (to PHP)
+print json.dumps(result)
--- src/routes.php
+++ src/routes.php
@@ -70,6 +70,24 @@ $app->get("/cari-nc/suhu-udara-laut/", function (Request $request, Response $res
     return $response->withJson(["status" => "success", "data_suhu_udara_laut" => $resultData], 200);
 });
 
+//cari data tinggi gelombang
+$app->get("/cari-nc/tinggi-gelombang/", function (Request $request, Response $response, $args){
+    $cari_lat = $request->getQueryParam("lat");
+    $cari_lon = $request->getQueryParam("lon");
+    $cari_tgl = $request->getQueryParam("tgl");
+
+    // Data yang akan dipindah ke python
+    $data = array($cari_lat,$cari_lon,$cari_tgl);
+
+    // Execute the python script with the JSON data
+    $result = shell_exec('python "/var/www/html/semar-api/src/API/tinggi_gelombang.py" ' . base64_encode(json_encode($data)));
+
+    // Decode the result
+    $resultData = json_decode($result, true);
+
+    return $response->withJson(["status" => "success", "data_tinggi_gelombang" => $resultData], 200);
+});
+
 
 
 //------------------------------------------------------------------------------------------------
@@ -125,7 +143,46 @@ $app->get("/cari-nc/angin/arus/suhu-udara-laut/", function (Request $request, Re
     // Execute the python script with the JSON data
     $result3 = shell_exec('python "/var/www/html/semar-api/src/API/suhu_udara_laut.py" ' . base64_encode(json_encode($data)));
     // Decode the result
-    $resultData3 = json_decode($result, true);
+    $resultData3 = json_decode($result3, true);
 
     return $response->withJson(["status" => "success", "data_angin" => $resultData,"data_arus"=>$resultData2, "data_suhu_udara_laut" => $resultData3], 200);
+});
+
+
+//------------------------------------------------------------------------------------------------
+//cari dari NC
+//empat parameter
+$app->get("/cari-nc/angin/arus/suhu-udara-laut/tinggi-gelombang/", function (Request $request, Response $response, $args){
+    $cari_lat = $request->getQueryParam("lat");
+    $cari_lon = $request->getQueryParam("lon");
+    $cari_tgl = $request->getQueryParam("tgl");
+
+    // Data yang akan dipindah ke python
+    $data = array($cari_lat,$cari_lon,$cari_tgl);
+
+    //angin
+    // Execute the python script with the JSON data
+    $result = shell_exec('python "/var/www/html/semar-api/src/API/angin.py" ' . base64_encode(json_encode($data)));
+    // Decode the result
+    $resultData = json_decode($result, true);
+
+    //arus
+    // Execute the python script with the JSON data
+    $result2 = shell_exec('python "/var/www/html/semar-api/src/API/arus.py" ' . base64_encode(json_encode($data)));
+    // Decode the result
+    $resultData2 = json_decode($result2, true);
+
+    //suhu udara laut
+    // Execute the python script with the JSON data
+    $result3 = shell_exec('python "/var/www/html/semar-api/src/API/suhu_udara_laut.py" ' . base64_encode(json_encode($data)));
+    // Decode the result
+    $resultData3 = json_decode($result3, true);
+
+    //tinggi gelombang
+     // Execute the python script with the JSON data
+    $result4 = shell_exec('python "/var/www/html/semar-api/src/API/tinggi_gelombang.py" ' . base64_encode(json_encode($data)));
+    // Decode the result
+    $resultData4 = json_decode($result4, true);
+
+    return $response->withJson(["status" => "success", "data_angin" => $resultData,"data_arus"=>$resultData2, "data_suhu_udara_laut" => $resultData3, "data_tinggi_gelombang" => $resultData4], 200);
 });
